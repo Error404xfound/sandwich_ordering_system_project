3d5a14f0f8337a171ed8ddc9399114aaa83fa99f
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-07-12 09:13:36 +0800

    refactor(data): split get methods into single-object and getAll variants
    - Add getX(int ID) lookups (linear search by ID) for models
    - Keep getAllX() as the Vector-returning getter for each field

--- src/data/DataStorage.java
+++ src/data/DataStorage.java
@@ -2,521 +2,253 @@ package data;
 
 import java.util.Vector;
 
-import data.Customer;
-import data.ItemStock;
-import data.Staff;
-import data.Order;
-import data.Ingredient;
-import data.MenuItem;
-import data.BreadType;
 
 public class DataStorage {
 
 	
 	private Vector<Customer> customers;
-	
-	private int nextMenuItemID;
-	
-	private int nextOrderID;
-	/**
-	 * 
-	 */
 	private Vector<LoyaltyDiscountTier> loyaltyDiscountTiers;
-	/**
-	 * 
-	 */
-	private int nextDiscountID;
-	/**
-	 * 
-	 */
 	private Vector<ItemStock> itemStock;
-	/**
-	 * 
-	 */
-	private int nextItemStockID;
-	/**
-	 * 
-	 */
 	private Vector<BreadType> breadTypes;
-	/**
-	 * 
-	 */
 	private Vector<Staff> staff;
-	/**
-	 * 
-	 */
-	private int nextTierID;
-	/**
-	 * 
-	 */
 	private Vector<LoyaltyDiscount> loyaltyDiscounts;
-	/**
-	 * 
-	 */
-	private int nextUserID;
-	/**
-	 * 
-	 */
 	private Vector<Order> orders;
-	/**
-	 * 
-	 */
 	private Vector<Ingredient> ingredients;
-	/**
-	 * 
-	 */
+
+	private int nextMenuItemID;
+	private int nextOrderID;
+	private int nextDiscountID;
+	private int nextItemStockID;
+	private int nextTierID;
+	private int nextUserID;
 	private int nextSandwichID;
-	/**
-	 * Getter of customers
-	 */
-	public Vector<Customer> getCustomers() {
+	
+	public Customer getCustomer(int ID) {
+		for (int i = 0; i < customers.size(); i++) {
+			Customer customer = customers.get(i);
+			if (customer.getUserID() == ID) {
+				return customer;
+			}
+		}
+		return null;
+	}
+	public Vector<Customer> getAllCustomers() {
 	 	 return customers; 
 	}
-	/**
-	 * Setter of customers
-	 */
 	public void setCustomers(Vector<Customer> customers) { 
 		 this.customers = customers; 
 	}
-	/**
-	 * Getter of ingredient
-	 */
 	
 	
-	/**
-	 * Getter of nextMenuItemID
-	 */
 	public int getNextMenuItemID() {
 	 	 return nextMenuItemID; 
 	}
-	/**
-	 * Setter of nextMenuItemID
-	 */
 	public void setNextMenuItemID(int nextMenuItemID) { 
 		 this.nextMenuItemID = nextMenuItemID; 
 	}
-	/**
-	 * Getter of nextOrderID
-	 */
 	public int getNextOrderID() {
 	 	 return nextOrderID; 
 	}
-	/**
-	 * Setter of nextOrderID
-	 */
 	public void setNextOrderID(int nextOrderID) { 
 		 this.nextOrderID = nextOrderID; 
 	}
-	/**
-	 * Getter of loyaltyDiscountTiers
-	 */
-	public Vector<LoyaltyDiscountTier> getLoyaltyDiscountTiers() {
+	public LoyaltyDiscountTier getLoyaltyDiscountTier(int ID) {
+		for (int i = 0; i < loyaltyDiscountTiers.size(); i++) {
+			LoyaltyDiscountTier loyaltyDiscountTier = loyaltyDiscountTiers.get(i);
+			if (loyaltyDiscountTier.getTierID() == ID) {
+				return loyaltyDiscountTier;
+			}
+		}
+		return null;
+	}
+	public Vector<LoyaltyDiscountTier> getAllLoyaltyDiscountTiers() {
 	 	 return loyaltyDiscountTiers; 
 	}
-	/**
-	 * Setter of loyaltyDiscountTiers
-	 */
 	public void setLoyaltyDiscountTiers(Vector<LoyaltyDiscountTier> loyaltyDiscountTiers) { 
 		 this.loyaltyDiscountTiers = loyaltyDiscountTiers; 
 	}
-	/**
-	 * Getter of nextDiscountID
-	 */
 	public int getNextDiscountID() {
 	 	 return nextDiscountID; 
 	}
-	/**
-	 * Setter of nextDiscountID
-	 */
 	public void setNextDiscountID(int nextDiscountID) { 
 		 this.nextDiscountID = nextDiscountID; 
 	}
-	/**
-	 * Getter of itemStock
-	 */
-	public Vector<ItemStock> getItemStock() {
+	public ItemStock getItemStock(int ID) {
+		for (int i = 0; i < itemStock.size(); i++) {
+			ItemStock itemStockObj = itemStock.get(i);
+			if (itemStockObj.getItemStockID() == ID) {
+				return itemStockObj;
+			}
+		}
+		return null;
+	}
+	public Vector<ItemStock> getAllItemStock() {
 	 	 return itemStock; 
 	}
-	/**
-	 * Setter of itemStock
-	 */
 	public void setItemStock(Vector<ItemStock> itemStock) { 
 		 this.itemStock = itemStock; 
 	}
-	/**
-	 * Getter of nextItemStockID
-	 */
 	public int getNextItemStockID() {
 	 	 return nextItemStockID; 
 	}
-	/**
-	 * Setter of nextItemStockID
-	 */
 	public void setNextItemStockID(int nextItemStockID) { 
 		 this.nextItemStockID = nextItemStockID; 
 	}
-	/**
-	 * Getter of breadTypes
-	 */
-	public Vector<BreadType> getBreadTypes() {
+	public BreadType getBreadType(int ID) {
+		for (int i = 0; i < breadTypes.size(); i++) {
+			BreadType breadType = breadTypes.get(i);
+			if (breadType.getMenuItemID() == ID) {
+				return breadType;
+			}
+		}
+		return null;
+	}
+	public Vector<BreadType> getAllBreadTypes() {
 	 	 return breadTypes; 
 	}
-	/**
-	 * Setter of breadTypes
-	 */
 	public void setBreadTypes(Vector<BreadType> breadTypes) { 
 		 this.breadTypes = breadTypes; 
 	}
-	/**
-	 * Getter of staff
-	 */
-	public Vector<Staff> getStaff() {
+	public Staff getStaff(int ID) {
+		for (int i = 0; i < staff.size(); i++) {
+			Staff staffObj = staff.get(i);
+			if (staffObj.getUserID() == ID) {
+				return staffObj;
+			}
+		}
+		return null;
+	}
+	public Vector<Staff> getAllStaff() {
 	 	 return staff; 
 	}
-	/**
-	 * Setter of staff
-	 */
 	public void setStaff(Vector<Staff> staff) { 
 		 this.staff = staff; 
 	}
-	/**
-	 * Getter of nextTierID
-	 */
 	public int getNextTierID() {
 	 	 return nextTierID; 
 	}
-	/**
-	 * Setter of nextTierID
-	 */
 	public void setNextTierID(int nextTierID) { 
 		 this.nextTierID = nextTierID; 
 	}
-	/**
-	 * Getter of loyaltyDiscounts
-	 */
-	public Vector<LoyaltyDiscount> getLoyaltyDiscounts() {
+	public LoyaltyDiscount getLoyaltyDiscount(int ID) {
+		for (int i = 0; i < loyaltyDiscounts.size(); i++) {
+			LoyaltyDiscount loyaltyDiscount = loyaltyDiscounts.get(i);
+			if (loyaltyDiscount.getDiscountID() == ID) {
+				return loyaltyDiscount;
+			}
+		}
+		return null;
+	}
+	public Vector<LoyaltyDiscount> getAllLoyaltyDiscounts() {
 	 	 return loyaltyDiscounts; 
 	}
-	/**
-	 * Setter of loyaltyDiscounts
-	 */
 	public void setLoyaltyDiscounts(Vector<LoyaltyDiscount> loyaltyDiscounts) { 
 		 this.loyaltyDiscounts = loyaltyDiscounts; 
 	}
-	/**
-	 * Getter of nextUserID
-	 */
 	public int getNextUserID() {
 	 	 return nextUserID; 
 	}
-	/**
-	 * Setter of nextUserID
-	 */
 	public void setNextUserID(int nextUserID) { 
 		 this.nextUserID = nextUserID; 
 	}
-	/**
-	 * Getter of orders
-	 */
-	public Vector<Order> getOrders() {
+	public Order getOrder(int ID) {
+		for (int i = 0; i < orders.size(); i++) {
+			Order order = orders.get(i);
+			if (order.getOrderID() == ID) {
+				return order;
+			}
+		}
+		return null;
+	}
+	public Vector<Order> getAllOrders() {
 	 	 return orders; 
 	}
-	/**
-	 * Setter of orders
-	 */
 	public void setOrders(Vector<Order> orders) { 
 		 this.orders = orders; 
 	}
-	/**
-	 * Getter of ingredients
-	 */
-	public Vector<Ingredient> getIngredients() {
+	public Ingredient getIngredient(int ID) {
+		for (int i = 0; i < ingredients.size(); i++) {
+			Ingredient ingredient = ingredients.get(i);
+			if (ingredient.getMenuItemID() == ID) {
+				return ingredient;
+			}
+		}
+		return null;
+	}
+	public Vector<Ingredient> getAllIngredients() {
 	 	 return ingredients; 
 	}
-	/**
-	 * Setter of ingredients
-	 */
 	public void setIngredients(Vector<Ingredient> ingredients) { 
 		 this.ingredients = ingredients; 
 	}
 	
 	
 	public void deleteLoyaltyDiscount(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public Ingredient getAllIngredients() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param staff 
-	 */
 	public void addStaff(Staff staff) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param breadType 
-	 */
 	public void addBreadType(BreadType breadType) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public MenuItem getAllMenuItems() { 
-		// TODO Auto-generated method
+	public Vector<MenuItem> getAllMenuItems() { 
 		return null;
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 * @param loyaltyDiscountTier 
-	 */
 	public void editLoyaltyDiscountTier(int ID, LoyaltyDiscountTier loyaltyDiscountTier) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 * @param customer 
-	 */
 	public void editCustomer(int ID, Customer customer) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteItemStock(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 * @param order 
-	 */
 	public void editOrder(int ID, Order order) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public LoyaltyDiscountTier getAllLoyaltyDiscountTiers() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteLoyaltyDiscountTier(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public BreadType getAllBreadTypes() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public Staff getAllStaff() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param loyaltyDiscount 
-	 * @param ID 
-	 */
 	
-	/**
-	 * 
-	 * @param ingredient 
-	 */
 	public void addIngredient(Ingredient ingredient) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public Customer getAllCustomers() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param customer 
-	 */
 	public void addCustomer(Customer customer) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteIngredient(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param itemStock 
-	 */
 	public void addItemStock(ItemStock itemStock) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ingredient 
-	 * @param ID 
-	 */
 	
-	/**
-	 * 
-	 * @return 
-	 */
-	public Order getAllOrders() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param ID 
-	 * @param breadType 
-	 */
 	public void editBreadType(int ID, BreadType breadType) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteStaff(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param order 
-	 */
 	public void addOrder(Order order) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public ItemStock getAllItemStock() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteCustomer(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param loyaltyDiscount 
-	 */
 	public void addLoyaltyDiscount(LoyaltyDiscount loyaltyDiscount) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteBreadType(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @return 
-	 */
-	public LoyaltyDiscount getAllLoyaltyDiscounts() { 
-		// TODO Auto-generated method
-		return null;
-	 }
-	/**
-	 * 
-	 * @param ID 
-	 */
 	public void deleteOrder(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param loyaltyDiscountTier 
-	 */
 	public void addLoyaltyDiscountTier(LoyaltyDiscountTier loyaltyDiscountTier) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * Getter of nextSandwichID
-	 */
 	public int getNextSandwichID() {
 	 	 return nextSandwichID; 
 	}
-	/**
-	 * Setter of nextSandwichID
-	 */
 	public void setNextSandwichID(int nextSandwichID) { 
 		 this.nextSandwichID = nextSandwichID; 
 	}
-	/**
-	 * 
-	 * @param ID 
-	 * @param staff 
-	 */
 	public void editStaff(int ID, Staff staff) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param breadType 
-	 * @param ID 
-	 */
 	 
 	
 	public void deleteSandwich(int ID) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param ID 
-	 * @param sandwich 
-	 */
 	public void editSandwich(int ID, Sandwich sandwich) { 
-		// TODO Auto-generated method
 	 }
-	/**
-	 * 
-	 * @param sandwich 
-	 */
 	public void addSandwich(Sandwich sandwich) { 
-		// TODO Auto-generated method
 	 }
 	
 	public void editIngredient(int ID, Ingredient ingredient) { 
-		// TODO Auto-generated method
 	 }
 	
 	public void editItemStock(int ID, ItemStock itemStock) { 
-		// TODO Auto-generated method
 	 } 
 	
 	public void editLoyaltyDiscount(int ID, LoyaltyDiscount loyaltyDiscount) {
