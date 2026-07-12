22d60eb2d61873d98dd3bdcc795999343b5c986a
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-07-12 19:32:01 +0800

    feat(ui): design sidebar panel for customer and staff nav

--- src/gui/CustomerSidebarScreen.java
+++ src/gui/CustomerSidebarScreen.java
@@ -3,6 +3,9 @@ package gui;
 import javax.swing.JPanel;
 
 import controller.MainFrame;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Font;
 
 public class CustomerSidebarScreen extends JPanel {
 
@@ -11,7 +14,39 @@ public class CustomerSidebarScreen extends JPanel {
 	private MainFrame main;
 
 	public CustomerSidebarScreen(MainFrame main) {
+		setBackground(new Color(192, 192, 192));
 		this.main = main;
+		setLayout(null);
+		
+		JButton btnOrder = new JButton("Order");
+		btnOrder.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnOrder.setBackground(Color.LIGHT_GRAY);
+		btnOrder.setBounds(0, 0, 240, 80);
+		add(btnOrder);
+		
+		JButton btnOrderAgain = new JButton("Order Again");
+		btnOrderAgain.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnOrderAgain.setBackground(Color.LIGHT_GRAY);
+		btnOrderAgain.setBounds(0, 80, 240, 80);
+		add(btnOrderAgain);
+		
+		JButton btnCurrentOrders = new JButton("Current Orders");
+		btnCurrentOrders.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnCurrentOrders.setBackground(Color.LIGHT_GRAY);
+		btnCurrentOrders.setBounds(0, 160, 240, 80);
+		add(btnCurrentOrders);
+		
+		JButton btnLoyaltyDiscounts = new JButton("Loyalty Discounts");
+		btnLoyaltyDiscounts.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnLoyaltyDiscounts.setBackground(Color.LIGHT_GRAY);
+		btnLoyaltyDiscounts.setBounds(0, 240, 240, 80);
+		add(btnLoyaltyDiscounts);
+		
+		JButton btnAccount = new JButton("My Account");
+		btnAccount.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnAccount.setBackground(Color.LIGHT_GRAY);
+		btnAccount.setBounds(0, 640, 240, 80);
+		add(btnAccount);
 	}
 
 	public void showCurrentOrders() {
--- src/gui/StaffSidebarScreen.java
+++ src/gui/StaffSidebarScreen.java
@@ -3,6 +3,10 @@ package gui;
 import javax.swing.JPanel;
 
 import controller.MainFrame;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
 
 public class StaffSidebarScreen extends JPanel {
 
@@ -11,7 +15,45 @@ public class StaffSidebarScreen extends JPanel {
 	private MainFrame main;
 
 	public StaffSidebarScreen(MainFrame main) {
+		setBackground(new Color(192, 192, 192));
 		this.main = main;
+		setLayout(null);
+		
+		JButton btnMenu = new JButton("Menu\r\n");
+		btnMenu.setBackground(new Color(192, 192, 192));
+		btnMenu.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnMenu.setBounds(0, 0, 240, 80);
+		add(btnMenu);
+		
+		JButton btnInventory = new JButton("Inventory");
+		btnInventory.setBackground(new Color(192, 192, 192));
+		btnInventory.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnInventory.setBounds(0, 80, 240, 80);
+		add(btnInventory);
+		
+		JButton btnOrders = new JButton("Orders");
+		btnOrders.setBackground(new Color(192, 192, 192));
+		btnOrders.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnOrders.setBounds(0, 160, 240, 80);
+		add(btnOrders);
+		
+		JButton btnAnalytics = new JButton("Analytics");
+		btnAnalytics.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnAnalytics.setBackground(Color.LIGHT_GRAY);
+		btnAnalytics.setBounds(0, 240, 240, 80);
+		add(btnAnalytics);
+		
+		JButton btnTier = new JButton("Loyalty Discount Manager");
+		btnTier.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnTier.setBackground(Color.LIGHT_GRAY);
+		btnTier.setBounds(0, 320, 240, 80);
+		add(btnTier);
+		
+		JButton btnStaff = new JButton("My Account");
+		btnStaff.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		btnStaff.setBackground(Color.LIGHT_GRAY);
+		btnStaff.setBounds(0, 640, 240, 80);
+		add(btnStaff);
 	}
 
 	public void showStaffAccount() {
