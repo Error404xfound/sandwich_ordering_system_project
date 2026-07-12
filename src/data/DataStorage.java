9f9667b6c5e6e70640a27b46ff974e857d86a322
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-07-12 09:20:05 +0800

    refactor(data): group DataStorage methods by model

--- src/data/DataStorage.java
+++ src/data/DataStorage.java
@@ -22,7 +22,9 @@ public class DataStorage {
 	private int nextTierID;
 	private int nextUserID;
 	private int nextSandwichID;
-	
+
+
+	// ===== Customer =====
 	public Customer getCustomer(int ID) {
 		for (int i = 0; i < customers.size(); i++) {
 			Customer customer = customers.get(i);
@@ -38,77 +40,15 @@ public class DataStorage {
 	public void setCustomers(Vector<Customer> customers) { 
 		 this.customers = customers; 
 	}
-	
-	
-	public int getNextMenuItemID() {
-	 	 return nextMenuItemID; 
-	}
-	public void setNextMenuItemID(int nextMenuItemID) { 
-		 this.nextMenuItemID = nextMenuItemID; 
-	}
-	public int getNextOrderID() {
-	 	 return nextOrderID; 
-	}
-	public void setNextOrderID(int nextOrderID) { 
-		 this.nextOrderID = nextOrderID; 
-	}
-	public LoyaltyDiscountTier getLoyaltyDiscountTier(int ID) {
-		for (int i = 0; i < loyaltyDiscountTiers.size(); i++) {
-			LoyaltyDiscountTier loyaltyDiscountTier = loyaltyDiscountTiers.get(i);
-			if (loyaltyDiscountTier.getTierID() == ID) {
-				return loyaltyDiscountTier;
-			}
-		}
-		return null;
-	}
-	public Vector<LoyaltyDiscountTier> getAllLoyaltyDiscountTiers() {
-	 	 return loyaltyDiscountTiers; 
-	}
-	public void setLoyaltyDiscountTiers(Vector<LoyaltyDiscountTier> loyaltyDiscountTiers) { 
-		 this.loyaltyDiscountTiers = loyaltyDiscountTiers; 
-	}
-	public int getNextDiscountID() {
-	 	 return nextDiscountID; 
-	}
-	public void setNextDiscountID(int nextDiscountID) { 
-		 this.nextDiscountID = nextDiscountID; 
-	}
-	public ItemStock getItemStock(int ID) {
-		for (int i = 0; i < itemStock.size(); i++) {
-			ItemStock itemStockObj = itemStock.get(i);
-			if (itemStockObj.getItemStockID() == ID) {
-				return itemStockObj;
-			}
-		}
-		return null;
-	}
-	public Vector<ItemStock> getAllItemStock() {
-	 	 return itemStock; 
-	}
-	public void setItemStock(Vector<ItemStock> itemStock) { 
-		 this.itemStock = itemStock; 
-	}
-	public int getNextItemStockID() {
-	 	 return nextItemStockID; 
-	}
-	public void setNextItemStockID(int nextItemStockID) { 
-		 this.nextItemStockID = nextItemStockID; 
-	}
-	public BreadType getBreadType(int ID) {
-		for (int i = 0; i < breadTypes.size(); i++) {
-			BreadType breadType = breadTypes.get(i);
-			if (breadType.getMenuItemID() == ID) {
-				return breadType;
-			}
-		}
-		return null;
-	}
-	public Vector<BreadType> getAllBreadTypes() {
-	 	 return breadTypes; 
-	}
-	public void setBreadTypes(Vector<BreadType> breadTypes) { 
-		 this.breadTypes = breadTypes; 
-	}
+	public void addCustomer(Customer customer) { 
+	 }
+	public void editCustomer(int ID, Customer customer) { 
+	 }
+	public void deleteCustomer(int ID) { 
+	 }
+
+
+	// ===== Staff =====
 	public Staff getStaff(int ID) {
 		for (int i = 0; i < staff.size(); i++) {
 			Staff staffObj = staff.get(i);
@@ -124,33 +64,24 @@ public class DataStorage {
 	public void setStaff(Vector<Staff> staff) { 
 		 this.staff = staff; 
 	}
-	public int getNextTierID() {
-	 	 return nextTierID; 
-	}
-	public void setNextTierID(int nextTierID) { 
-		 this.nextTierID = nextTierID; 
-	}
-	public LoyaltyDiscount getLoyaltyDiscount(int ID) {
-		for (int i = 0; i < loyaltyDiscounts.size(); i++) {
-			LoyaltyDiscount loyaltyDiscount = loyaltyDiscounts.get(i);
-			if (loyaltyDiscount.getDiscountID() == ID) {
-				return loyaltyDiscount;
-			}
-		}
-		return null;
-	}
-	public Vector<LoyaltyDiscount> getAllLoyaltyDiscounts() {
-	 	 return loyaltyDiscounts; 
-	}
-	public void setLoyaltyDiscounts(Vector<LoyaltyDiscount> loyaltyDiscounts) { 
-		 this.loyaltyDiscounts = loyaltyDiscounts; 
-	}
+	public void addStaff(Staff staff) { 
+	 }
+	public void editStaff(int ID, Staff staff) { 
+	 }
+	public void deleteStaff(int ID) { 
+	 }
+
+
+	// ===== Shared User ID counter (Customer / Staff) =====
 	public int getNextUserID() {
 	 	 return nextUserID; 
 	}
 	public void setNextUserID(int nextUserID) { 
 		 this.nextUserID = nextUserID; 
 	}
+
+
+	// ===== Order =====
 	public Order getOrder(int ID) {
 		for (int i = 0; i < orders.size(); i++) {
 			Order order = orders.get(i);
@@ -166,6 +97,51 @@ public class DataStorage {
 	public void setOrders(Vector<Order> orders) { 
 		 this.orders = orders; 
 	}
+	public void addOrder(Order order) { 
+	 }
+	public void editOrder(int ID, Order order) { 
+	 }
+	public void deleteOrder(int ID) { 
+	 }
+	public int getNextOrderID() {
+	 	 return nextOrderID; 
+	}
+	public void setNextOrderID(int nextOrderID) { 
+		 this.nextOrderID = nextOrderID; 
+	}
+
+
+	// ===== ItemStock =====
+	public ItemStock getItemStock(int ID) {
+		for (int i = 0; i < itemStock.size(); i++) {
+			ItemStock itemStockObj = itemStock.get(i);
+			if (itemStockObj.getItemStockID() == ID) {
+				return itemStockObj;
+			}
+		}
+		return null;
+	}
+	public Vector<ItemStock> getAllItemStock() {
+	 	 return itemStock; 
+	}
+	public void setItemStock(Vector<ItemStock> itemStock) { 
+		 this.itemStock = itemStock; 
+	}
+	public void addItemStock(ItemStock itemStock) { 
+	 }
+	public void editItemStock(int ID, ItemStock itemStock) { 
+	 } 
+	public void deleteItemStock(int ID) { 
+	 }
+	public int getNextItemStockID() {
+	 	 return nextItemStockID; 
+	}
+	public void setNextItemStockID(int nextItemStockID) { 
+		 this.nextItemStockID = nextItemStockID; 
+	}
+
+
+	// ===== Ingredient =====
 	public Ingredient getIngredient(int ID) {
 		for (int i = 0; i < ingredients.size(); i++) {
 			Ingredient ingredient = ingredients.get(i);
@@ -181,52 +157,59 @@ public class DataStorage {
 	public void setIngredients(Vector<Ingredient> ingredients) { 
 		 this.ingredients = ingredients; 
 	}
-	
-	
-	public void deleteLoyaltyDiscount(int ID) { 
-	 }
-	public void addStaff(Staff staff) { 
-	 }
-	public void addBreadType(BreadType breadType) { 
-	 }
-	public Vector<MenuItem> getAllMenuItems() { 
-		return null;
-	 }
-	public void editLoyaltyDiscountTier(int ID, LoyaltyDiscountTier loyaltyDiscountTier) { 
-	 }
-	public void editCustomer(int ID, Customer customer) { 
-	 }
-	public void deleteItemStock(int ID) { 
-	 }
-	public void editOrder(int ID, Order order) { 
-	 }
-	public void deleteLoyaltyDiscountTier(int ID) { 
-	 }
-	
 	public void addIngredient(Ingredient ingredient) { 
 	 }
-	public void addCustomer(Customer customer) { 
+	public void editIngredient(int ID, Ingredient ingredient) { 
 	 }
 	public void deleteIngredient(int ID) { 
 	 }
-	public void addItemStock(ItemStock itemStock) { 
+
+
+	// ===== BreadType =====
+	public BreadType getBreadType(int ID) {
+		for (int i = 0; i < breadTypes.size(); i++) {
+			BreadType breadType = breadTypes.get(i);
+			if (breadType.getMenuItemID() == ID) {
+				return breadType;
+			}
+		}
+		return null;
+	}
+	public Vector<BreadType> getAllBreadTypes() {
+	 	 return breadTypes; 
+	}
+	public void setBreadTypes(Vector<BreadType> breadTypes) { 
+		 this.breadTypes = breadTypes; 
+	}
+	public void addBreadType(BreadType breadType) { 
 	 }
-	
 	public void editBreadType(int ID, BreadType breadType) { 
-	 }
-	public void deleteStaff(int ID) { 
-	 }
-	public void addOrder(Order order) { 
-	 }
-	public void deleteCustomer(int ID) { 
-	 }
-	public void addLoyaltyDiscount(LoyaltyDiscount loyaltyDiscount) { 
 	 }
 	public void deleteBreadType(int ID) { 
 	 }
-	public void deleteOrder(int ID) { 
+
+
+	// ===== Shared MenuItem ID counter (Ingredient / BreadType / MenuItem) =====
+	public int getNextMenuItemID() {
+	 	 return nextMenuItemID; 
+	}
+	public void setNextMenuItemID(int nextMenuItemID) { 
+		 this.nextMenuItemID = nextMenuItemID; 
+	}
+
+
+	// ===== MenuItem =====
+	public Vector<MenuItem> getAllMenuItems() { 
+		return null;
 	 }
-	public void addLoyaltyDiscountTier(LoyaltyDiscountTier loyaltyDiscountTier) { 
+
+
+	// ===== Sandwich =====
+	public void addSandwich(Sandwich sandwich) { 
+	 }
+	public void editSandwich(int ID, Sandwich sandwich) { 
+	 }
+	public void deleteSandwich(int ID) { 
 	 }
 	public int getNextSandwichID() {
 	 	 return nextSandwichID; 
@@ -234,25 +217,65 @@ public class DataStorage {
 	public void setNextSandwichID(int nextSandwichID) { 
 		 this.nextSandwichID = nextSandwichID; 
 	}
-	public void editStaff(int ID, Staff staff) { 
+
+
+	// ===== LoyaltyDiscount =====
+	public LoyaltyDiscount getLoyaltyDiscount(int ID) {
+		for (int i = 0; i < loyaltyDiscounts.size(); i++) {
+			LoyaltyDiscount loyaltyDiscount = loyaltyDiscounts.get(i);
+			if (loyaltyDiscount.getDiscountID() == ID) {
+				return loyaltyDiscount;
+			}
+		}
+		return null;
+	}
+	public Vector<LoyaltyDiscount> getAllLoyaltyDiscounts() {
+	 	 return loyaltyDiscounts; 
+	}
+	public void setLoyaltyDiscounts(Vector<LoyaltyDiscount> loyaltyDiscounts) { 
+		 this.loyaltyDiscounts = loyaltyDiscounts; 
+	}
+	public void addLoyaltyDiscount(LoyaltyDiscount loyaltyDiscount) { 
 	 }
-	 
-	
-	public void deleteSandwich(int ID) { 
-	 }
-	public void editSandwich(int ID, Sandwich sandwich) { 
-	 }
-	public void addSandwich(Sandwich sandwich) { 
-	 }
-	
-	public void editIngredient(int ID, Ingredient ingredient) { 
-	 }
-	
-	public void editItemStock(int ID, ItemStock itemStock) { 
-	 } 
-	
 	public void editLoyaltyDiscount(int ID, LoyaltyDiscount loyaltyDiscount) {
-		
+	 }
+	public void deleteLoyaltyDiscount(int ID) { 
+	 }
+	public int getNextDiscountID() {
+	 	 return nextDiscountID; 
+	}
+	public void setNextDiscountID(int nextDiscountID) { 
+		 this.nextDiscountID = nextDiscountID; 
+	}
+
+
+	// ===== LoyaltyDiscountTier =====
+	public LoyaltyDiscountTier getLoyaltyDiscountTier(int ID) {
+		for (int i = 0; i < loyaltyDiscountTiers.size(); i++) {
+			LoyaltyDiscountTier loyaltyDiscountTier = loyaltyDiscountTiers.get(i);
+			if (loyaltyDiscountTier.getTierID() == ID) {
+				return loyaltyDiscountTier;
+			}
+		}
+		return null;
+	}
+	public Vector<LoyaltyDiscountTier> getAllLoyaltyDiscountTiers() {
+	 	 return loyaltyDiscountTiers; 
+	}
+	public void setLoyaltyDiscountTiers(Vector<LoyaltyDiscountTier> loyaltyDiscountTiers) { 
+		 this.loyaltyDiscountTiers = loyaltyDiscountTiers; 
+	}
+	public void addLoyaltyDiscountTier(LoyaltyDiscountTier loyaltyDiscountTier) { 
+	 }
+	public void editLoyaltyDiscountTier(int ID, LoyaltyDiscountTier loyaltyDiscountTier) { 
+	 }
+	public void deleteLoyaltyDiscountTier(int ID) { 
+	 }
+	public int getNextTierID() {
+	 	 return nextTierID; 
+	}
+	public void setNextTierID(int nextTierID) { 
+		 this.nextTierID = nextTierID; 
 	}
 
 }
