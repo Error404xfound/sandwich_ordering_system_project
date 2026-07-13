30b5106c850bd031e4f592df3edf9ce2efff0e7f
Albus Esguerra <albus.esguerra.268@gmail.com>
2026-07-13 11:30:36 +0800

    refactor(gui): remove unused placholder methods from StaffSidebarScreen

--- src/gui/StaffSidebarScreen.java
+++ src/gui/StaffSidebarScreen.java
@@ -89,28 +89,4 @@ public class StaffSidebarScreen extends JPanel {
 		add(btnStaff);
 	}
 
-	public void showStaffAccount() {
-		// TODO Auto-generated method
-	}
-
-	public void showMenu() {
-		// TODO Auto-generated method
-	}
-
-	public void showInventory() {
-		// TODO Auto-generated method
-	}
-
-	public void showOrders() {
-		// TODO Auto-generated method
-	}
-
-	public void showAnalytics() {
-		// TODO Auto-generated method
-	}
-
-	public void showLoyaltyDiscountMng() {
-		// TODO Auto-generated method
-	}
-
 }
